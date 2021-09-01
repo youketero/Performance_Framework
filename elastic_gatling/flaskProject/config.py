d08333bf72cb0b427f773a6a9718cb9a0f837397
youketero <youketeroamano22@gmail.com>
2021-09-01 18:57:20 +0300

    Delete 2 flask project

--- elastic_gatling/flaskProject/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config(object):
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    FLASK_DEBUG = 1
-    POSTS_PER_PAGE = 5
