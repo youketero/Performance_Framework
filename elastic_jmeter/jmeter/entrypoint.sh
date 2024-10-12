64187d1f35532177959a0145e19c8aab02ec106b
youketero <youketeroamano22@gmail.com>
2024-10-12 10:06:36 +0300

    Update entrypoint.sh

--- elastic_jmeter/jmeter/entrypoint.sh
+++ elastic_jmeter/jmeter/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash ./bin/jmeter.sh -n -t ${SCENARIO} -j ${WORKSPACE}/jmeter_${BUILD_NUMBER}.log -l ${WORKSPACE}/report_${BUILD_NUMBER}.csv -e -o ${WORKSPACE}/report_${BUILD_NUMBER}/ ${PARAMETERS} -f
+ls
